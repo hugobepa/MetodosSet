1fc4e994d3cc44357254e2fd3f866fb626223e61
hubepa1516daw2 <hubepa1516daw2@gmail.com>
2016-06-14 22:49:08 +0200

    V0.3 GuardarBBDD

---
+++ src/metodosset/Parametrizar/Parametrizar.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodosset.Parametrizar;
+
+import java.util.TreeSet;
+
+/**
+ *
+ * @author hugo
+ */
+public class Parametrizar {
+    ComparadorPersona cp = new ComparadorPersona();
+    TreeSet<Persona>  arbol= new  TreeSet<Persona>();
+    
+}
